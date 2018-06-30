3e6c3b9f3b0012dbf0462df29781974362ec880b
ananya-agrawal <ananyaagrawal125@gmail.com>
2018-06-30 16:32:01 +0530

    heroku setup

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
