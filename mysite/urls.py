3265099e6a7926eaa147f802ee64647aa1be05e7
ananya-agrawal <ananyaagrawal125@gmail.com>
2018-07-03 21:23:25 +0530

    addind features

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import include
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
